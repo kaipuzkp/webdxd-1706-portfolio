2cd52f49f7175b9a530f70cdc75a6b145cc03bf0
kaipuzkp <kaipuzkp@outlook.com>
2017-07-07 18:22:54 -0700

    add update Function

--- js/main.js
+++ js/main.js
@@ -38,12 +38,15 @@ $("#user-container").on('click', '.user',function(event){
 
    		 $('.detail-info').empty()
 
+   		 $('#'+uid).empty('<h1>')
+   		 $('#'+uid).append($('<h1>').text(response.name))
    		 var detailInfo = $('<div class="detail-info">')
    		 $('<h2>').text(response.age).appendTo(detailInfo)
    		 $('<h2>').text(response.school).appendTo(detailInfo)
 		 $('<button>').addClass('delBtn').attr("id","delBtn").text("Delete").appendTo(detailInfo)
-   		
+   		 $('<button>').addClass('updBtn').attr("id","updBtn").text("Update").appendTo(detailInfo)
    		 $('#' + uid).append(detailInfo)
+
 
 	})
 
@@ -71,6 +74,7 @@ $("#user-container").on('click', '.delBtn',function(event){
 		     	dataType: 'json'
 			})
 			$('#'+uid).remove()
+			
 		}
 
 })
@@ -80,23 +84,53 @@ $("#user-container").on('click', '.delBtn',function(event){
 
 //Update Function////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// $("#user-container").on('click', '.delBtn',function(event){
-// 	var uid = $(this).parent().parent().attr('id')
-// 		console.log(uid)
-// 		console.log('clicked')
-// 		if(confirm('This action will permanently delete current Contract! Are you sure?')){
-  
-// 			$.ajax({
-// 				type: 'DELETE',
-// 				url: 'https://webdxd-student-api.herokuapp.com/student/'+uid,
-// 				success: function(data) { console.log(data) },
-// 		        contentType: "application/json",
-// 		     	dataType: 'json'
-// 			})
-// 			$('#'+uid).remove()
-// 		}
+$("#user-container").on('click', '.updBtn',function(event){
+	var uid = $(this).parent().parent().attr('id')
+	console.log('Update button clicked')
+	var newName = prompt("Please Enter Your New Name", "");
+	var newAge = prompt("Please Enter Your New Age", "");
+	var newSchool = prompt("Please Enter Your New School", "");
 
-// })
+
+	if(!(newName == null && newAge == null && newSchool == null) ){
+
+		$.get('https://webdxd-student-api.herokuapp.com/student/' + uid, function(response){
+
+			console.log(response)
+
+			if(newName == null){newName = response.name}
+			if(newAge== null){newAge = response.age};
+			if(newSchool == null){newSchool = response.school};
+
+
+		
+
+			if(confirm('This action will permanently change the information of current Contract! Are you sure?')){
+		  		var updStudent = {
+
+					"name": newName,
+					"age": newAge,
+					"school": newSchool
+
+			    }
+				$.ajax({
+					type: 'PUT',
+					url: 'https://webdxd-student-api.herokuapp.com/student/'+uid,
+					data: JSON.stringify(updStudent),
+					success: function(data) { console.log(data) },
+			        contentType: "application/json",
+			     	dataType: 'json'
+
+				})
+
+
+
+			}
+		})	
+	}
+
+})
+
 
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
